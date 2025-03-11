c682de9e61879485f1d15c5e8b93a87bcb3ce7f8
krishnachaitanya2005 <chaitanya.jgs@gmail.com>
2025-03-11 22:48:19 +0530

    support-component completed

--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -2,8 +2,405 @@ import React from "react";
 
 function CreateTicket() {
 	return (
-		<div>
-			<h1>Create Ticket</h1>
+		<div className="container pb-5 px-5 mb-5 me-0">
+			<div className="row">
+				<h4 className="py-4 mt-4 text-muted">
+					To create a ticket, select a relevant topic
+				</h4>
+				<div className="col-4">
+					<h4 className="py-4">
+						<i class="fa-regular fa-square-plus"></i> Account Opening
+					</h4>
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Resident individual
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Minor
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Non Resident Indian (NRI)
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Company, Partnership, HUF and LLP
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Glossary
+					</a>
+					{/* end */}
+					<h4 className="py-4">
+						<i class="fa-regular fa-credit-card"></i> Funds{" "}
+					</h4>
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Add money
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Withdraw money
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Add bank accounts
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						eMandates
+					</a>
+				</div>
+				<div className="col-4">
+					<h4 className="py-4">
+						<i class="fa-regular fa-user"></i> Your Zerodha Account
+					</h4>
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Your Profile
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Account modification
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Client Master Report (CMR) and Depository Participant (DP)
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Nomination
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Transfer and conversion of securities
+					</a>
+					{/* end */}
+					<h4 className="py-4">
+						<i class="fa-solid fa-c"></i> Console
+					</h4>
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Portfolio
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Corporate actions
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Funds statement
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Reports
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Profile
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Segments
+					</a>
+				</div>
+				<div className="col-4">
+					<h4 className="py-4">
+						<i class="fa-solid fa-chart-simple"></i> Kite
+					</h4>
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						IPO
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Trading FAQs
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Margin Trading Facility (MTF) and Margins
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Charts and orders{" "}
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Alerts and Nudges{" "}
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						General{" "}
+					</a>
+					{/* end */}
+					<h4 className="py-4">
+						<i class="fa-solid fa-coins"></i> Coin
+					</h4>
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Understanding mutual funds and Coin
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Coin app
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Coin web
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						Transactions and reports
+					</a>
+					<br />
+					<a
+						style={{
+							marginBottom: "10px",
+							textDecoration: "none",
+							color: "#387ed1",
+						}}
+						className="fw-normal"
+						href=""
+					>
+						National Pension Scheme (NPS)
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -2,8 +2,65 @@ import React from "react";
 
 function Hero() {
 	return (
-		<div>
-			<h1>Hero</h1>
+		<div
+			style={{ backgroundColor: "#387ed1", paddingLeft: "100px" }}
+			className="text-white fw-medium mx-0 pb-5"
+		>
+			<div className="row px-5 mx-5 pt-5 pb-5">
+				<div className="col-7">
+					<p className="fs-4 pb-3 fw-semibold">Support Portal</p>{" "}
+					<p className="fs-3">
+						Search for an answer or browse help topics to create a ticket
+					</p>
+					<form class="d-flex" role="search">
+						<input
+							class="form-control me-2 rounded-0"
+							type="search"
+							placeholder="Eg: how do i activate F&O, why is my order getting rejected ..."
+							aria-label="Search"
+							style={{ height: "50px", width: "600px" }}
+						></input>
+						{/* <button class="btn btn-outline-success" type="submit">
+							Search
+						</button> */}
+					</form>
+					<div className="pt-3">
+						<a href="" style={{ color: "white", paddingRight: "25px" }}>
+							Track account opening
+						</a>
+						<a href="" style={{ color: "white", paddingRight: "25px" }}>
+							Track segment activation
+						</a>
+						<a href="" style={{ color: "white", paddingRight: "25px" }}>
+							Intraday margins
+						</a>
+						<br />
+						<a href="" style={{ color: "white", paddingRight: "25px" }}>
+							Kite user manual
+						</a>
+					</div>
+				</div>
+				<div className="col-4">
+					<p className="text-end pb-5 mb-5">
+						<a href="" style={{ color: "white" }}>
+							Track tickets{" "}
+						</a>
+					</p>
+					<p className="fs-5 fw-semibold">Featured</p>
+					<ol>
+						<li className=" pb-3">
+							<a href="" style={{ color: "white" }}>
+								Issue with order placement on Kite [Resolved]
+							</a>
+						</li>
+						<li>
+							<a href="" style={{ color: "white" }}>
+								Surveillance measure on scrips - March 2025
+							</a>
+						</li>
+					</ol>
+				</div>
+			</div>
 		</div>
 	);
 }
