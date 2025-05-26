f0eaf9404061c8ccff51ac74269e396e058ce7ef
krishnachaitanya2005 <chaitanya.jgs@gmail.com>
2025-05-26 14:09:52 +0530

    fixed some errors and cors

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require("../model/UserModel");
+const User = require("../models/UserModel");
 const { createSecretToken } = require("../util/SecretToken");
 const bcrypt = require("bcryptjs");
 
